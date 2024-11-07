e36aeac3c90cca22685d2a11037884ce5daf3c7e
Ruwangi19 <ruuprabo@gmail.com>
2024-11-07 13:12:34 +0530

    update code

---
+++ src/main/java/Student/Managment/System/Student/Management/System/Repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package Student.Managment.System.Student.Management.System.Repository;
+import Student.Managment.System.Student.Management.System.Entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student,Long>{
+    //crud
+
+    //crud complete
+}
